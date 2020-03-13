d886d984c138060cec3efd4f9457d17b5e874b5d
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-03-13 19:20:46 -0400

    added markers to map

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import { GOOGLE_API_KEY } from "./requests"
+import CurrentLocation from "./Map"
 
 const mapStyles = {
   width: '100%',
@@ -8,19 +9,49 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: 43.644260,
-         lng: -79.402260
-        }}
-      />
-    );
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+onClose = props => {
+  if (this.state.showingInfoWindow) {
+    this.setState({
+      showingInfoWindow: false,
+      activeMarker: null
+    });
   }
+};
+render() {
+  return (
+    <CurrentLocation
+    centerAroundCurrentLocation
+    google={this.props.google}
+    >
+      <Marker
+        onClick={this.onMarkerClick}
+        name={'Lighthouse Labs'}
+      />
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+      >
+        <div>
+          <h4>{this.state.selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+      </CurrentLocation>
+  );
+}
 }
 
 export default GoogleApiWrapper({

67bce9e0553b5adb55ca782531c4fe363fe6cc03
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-03-13 14:36:42 -0400

    added node skeleton

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const PORT       = process.env.PORT || 8080;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
 const app        = express();
+const cors       = require('cors');
 const morgan     = require('morgan');
 
 // PG database client/connection setup
@@ -20,15 +20,8 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-
-app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+app.use(cors());
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
@@ -47,7 +40,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("ok");
 });
 
 app.listen(PORT, () => {

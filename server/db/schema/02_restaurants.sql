5f4a32351d26a7dddf40542ec4c3bb9b43cb91af
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-03-14 13:29:52 -0400

    added tables to db

---
+++ server/db/schema/02_restaurants.sql
@@ -0,0 +1,11 @@
+-- DROP TABLE IF EXISTS restaurants CASCADE;
+DROP TABLE IF EXISTS restaurants CASCADE;
+
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY NOT NULL,
+  customer_id INTEGER REFERENCES customers(id),
+  name VARCHAR(255) NOT NULL,
+  start_time TIME,
+  end_time TIME,
+  address TEXT
+);

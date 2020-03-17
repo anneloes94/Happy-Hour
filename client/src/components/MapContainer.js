912b19620428fc0183f09a10a82da2375a0144fd
Anne <anneloes9405@hotmail.com>
2020-03-17 13:23:34 -0400

    added constructor, restaurantsMarker, changed axios a bit - not working, now following class-based axios request tutorial

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,39 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 import CurrentLocation from "./Map";
 import axios from "axios";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-
-// [...] RETRIEVES DATA FROM THE API DATABASE
-Promise.all([
-  Promise.resolve(axios.get("http://localhost:8080/api/customers")),
-  Promise.resolve(axios.get("http://localhost:8080/api/restaurants"))
-])
-  .then(all => {
-    const customers = all[0].data;
-    const restaurants = all[1].data;
-    console.log(customers);
-    console.log(restaurants)
-  })
-  .catch(error =>
-    console.log(
-      "An error occurred while retrieving data from the database",
-      error
-    )
-  );
-
-// const mapStyles = {
-//   width: "100%",
-//   height: "100%"
-// };
-
 export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false, //Hides or shows the infoWindow
-    activeMarker: {}, //Shows the active marker upon click
-    selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
-  };
+  // [...] RETRIEVES DATA FROM THE API DATABASE
+  // on componentLoad load data into state
+  // use componentDidMount
+  // when comopnent is loaded up onto the browser, it will grab the data then
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false, //Hides or shows the infoWindow
+      activeMarker: {}, //Shows the active marker upon click
+      selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
+      restaurants: []
+    };
+    console.log(this.state.restaurants);
+  }
+
+  componentDidMount() {
+    const customersData = axios.get("http://localhost:8080/api/customers");
+    const restaurantsData = axios.get("http://localhost:8080/api/restaurants");
+    Promise.all([customersData, restaurantsData])
+      .then(all => {
+        this.setState({
+          customers: all[0].data,
+          restaurants: all[1].data
+        });
+      })
+      .catch(error => {
+        console.log(
+          "An error occurred while retrieving data from the database",
+          error
+        );
+      });
+  }
 
   onMarkerClick = (props, marker, e) =>
     this.setState({
@@ -50,11 +53,16 @@ export class MapContainer extends Component {
       });
     }
   };
+  
+  // restaurantMarkers = this.state.restaurants.map(restaurant => { <Marker onClick={this.onMarkerClick} name={restaurant.name} /> })
+
   render() {
     return (
       <MuiThemeProvider>
         <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
-          <Marker onClick={this.onMarkerClick} name={"Lighthouse Labs"} />
+          {this.state.restaurants.length > 0 ? console.log("hello") : console.log("fail")}
+          {/* {(this.state.restaurants.length > 0) ? (this.restaurantMarkers) : ( <Marker onClick={this.onMarkerClick} name="Lighthouse Labs" /> )} */}
+          
           <InfoWindow
             marker={this.state.activeMarker}
             visible={this.state.showingInfoWindow}

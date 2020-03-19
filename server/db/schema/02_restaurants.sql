0ca787525f7cc6460502683bbfd9716c2d1d6680
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-03-18 21:36:51 -0400

    added new column to db, changed restaurant seeds to match, mapped through date_available in mapContainer.js

--- server/db/schema/02_restaurants.sql
+++ server/db/schema/02_restaurants.sql
@@ -1,10 +1,10 @@
--- DROP TABLE IF EXISTS restaurants CASCADE;
 DROP TABLE IF EXISTS restaurants CASCADE;
 
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INTEGER REFERENCES customers(id),
   name VARCHAR(255) NOT NULL,
+  date_available integer[],
   start_time TIME,
   end_time TIME,
   address TEXT,

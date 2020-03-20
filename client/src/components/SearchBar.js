2425db8ab69bede99dd58790afe712e1d21a0b69
Anneloes van Kesteren <50273660+anneloes94@users.noreply.github.com>
2020-03-20 12:40:07 -0400

    23 integrating searchbar into map (#18)
    * got rid of extra MuiThemeProvider
    
    * changed some things around - formatted document
    
    * searchbar now appears in the right spot, search function still doesnt work

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,12 +1,26 @@
-// // Imports
+// SEARCHBAR WITH SETTINGS LIKE COUNTRY SEARCH
 import React from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import "react-google-places-autocomplete/dist/assets/index.css";
+import "./SearchBar.css"
 
-export default function Search() {
+export default function Search(props) {
   return (
-    <div>
-      <GooglePlacesAutocomplete onSelect={console.log} />
+    <div className="searchBar">
+      {/* More info: 
+      https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest 
+      https://www.npmjs.com/package/react-google-places-autocomplete */}
+      <GooglePlacesAutocomplete 
+      autocompletionRequest={{
+        componentRestrictions: {
+          country: ["ca"]
+        },
+        // location: selectedLocation,
+        // radius: Number,
+
+      }} 
+      onSelect={(selectedLocation) => this.setState({selectedPlace: selectedLocation})}
+      placeholder={"Where to..?"} />
     </div>
   );
 }

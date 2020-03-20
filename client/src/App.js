a212a72b97520c84f884cc62e3146705c552f907
Anne <anneloes9405@hotmail.com>
2020-03-20 12:13:31 -0400

    searchbar now appears in the right spot, search function still doesnt work

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 import MapContainer from "./components/MapContainer";
-import Search from "./components/SearchBar";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import ResponsiveDrawer from "./components/NavBar";
 
 function App() {
   const [pins, addPin] = useState([]);
   return (
-   
     <MuiThemeProvider>
       <ResponsiveDrawer />
       <script
         url={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEYTWO}&libraries=places`}
       ></script>
-      <Search addPin={addPin} />
       <MapContainer pins={pins} />
     </MuiThemeProvider>
   );

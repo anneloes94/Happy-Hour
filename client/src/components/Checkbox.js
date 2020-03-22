dd027e9149142ce200bc4ff78548cc5f0e9f3953
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-03-22 18:05:22 -0400

    added marker to show current location

--- client/src/components/Checkbox.js
+++ client/src/components/Checkbox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import "./Checkbox.css"
+
 export default function Checkboxes() {
   const [checked, setChecked] = React.useState(true);
   const handleChange = event => {

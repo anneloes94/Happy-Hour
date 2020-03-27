449e91162a6dded2a14d8e8300ebf66da9b47495
Anneloes <anneloes9405@hotmail.com>
2020-03-27 16:43:45 -0400

    Login page styling fixed @cassandralin

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,92 +1,77 @@
 import React, { useState, Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import { Redirect } from 'react-router-dom'
-import "./Login.css"
-import NavBar from "./NavBar"
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Redirect } from "react-router-dom";
+import "./Login.css";
+// import NavBar from "./NavBar";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Logo from "./Photos/Logo2_white.svg";
 
 const drawerWidth = 240;
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {main: "#7C8C03" },
-    secondary: { main: '#f2b705' },
-    success: { main: "#4c5f00"},
-    contrastThreshold: 3, 
-    tonalOffset: 0.2,
-    action: {selected: "#fff"},
-  },
-  status: {
-    danger: { main: '#F21138'},
-  },
-  overrides: {
-    MuiButton: { // Name of the component ⚛️ / style sheet
-        root: { // Name of the rule
-            backgroundColor: '#7C8C03', // Some CSS
-            color: "#fff",
-
-        },
-    },
-},
-});
-
 export default function Login() {
   const [redirect, setState] = useState(false);
-  
+
   const setRedirect = () => {
     setState({
-      redirect: true,
-    })
+      redirect: true
+    });
   };
 
   const renderRedirect = () => {
     if (redirect) {
-      return <Redirect to ="/map" />
+      return <Redirect to="/map" />;
     }
   };
 
-        return (
-
-          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <NavBar />
-          <div className="container">
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                <div theme={theme} >
-                <form>
-                    <h3>Sign In</h3>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input type="email" className="form-control" placeholder="Enter username" />
-                    </div>
-        
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" />
-                    </div>
-        
-                    <div className="form-group">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                        </div>
-                    </div>
-                    
-                    {renderRedirect()}
-                    <button onClick={setRedirect}>Submit</button>
-                    <p className="forgot-password text-right">
-                        Forgot <a href="#">password?</a>
-                    </p>
-                </form>
-                </div>
-                </li>
-                <li className="nav-item">
-                </li>
-              </ul>
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            <img src={Logo} height="50EM" position="center"></img>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="container" id="navbarTogglerDemo02">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+        <div >
+        <form className="form-group">
+            <h3>Sign In</h3>
+            <div className="form-element">
+                <label>Username</label>
+                <input type="email" className="form-control" placeholder="Enter username" />
             </div>
-          </div>
-        </nav>
-         
-        );
-};
+
+            <div className="form-element">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+            </div>
+
+            <div className="form-element">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+            </div>
+            
+            {renderRedirect()}
+            <button className="submit-button" onClick={setRedirect}>Submit</button>
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+        </form>
+        </div>
+        </li>
+        <li className="nav-item">
+        </li>
+      </ul>
+    </div>
+</div>
+  );
+}
